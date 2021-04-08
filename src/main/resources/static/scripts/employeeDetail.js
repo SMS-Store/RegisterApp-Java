3793ef20c78bb23a28c5b5f824b8005cd6746c0a
fxnggg <74802761+fxnggg@users.noreply.github.com>
2021-04-08 11:05:06 -0500

    Validation with focus and select; some code to start ajax stuff

--- src/main/resources/static/scripts/employeeDetail.js
+++ src/main/resources/static/scripts/employeeDetail.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event)
 {
-	// Empty
+	getSaveActionElement().addEventListener("click", saveActionClick);
 });
 
-function validateForm()
+/*function validateForm()
 {
 	let firstname = document.forms["createEmployee"]["firstname"].value;
 	let lastname = document.forms["createEmployee"]["lastname"].value;
@@ -24,4 +24,80 @@ function validateForm()
 	{
 		return true;
 	}
+}*/
+
+function validateSave(event) {
+    const firstname = getFirstName();
+    const lastname = getLastName();
+    const password = getPassword();
+    const verifiedPassword = getVerifiedPassword();
+
+    if(firstname == "") {
+        displayError("Please enter your first name.");
+        getFirstName().focus();
+        getFirstName().select();
+        return false;
+    }
+
+    if(lastname == "") {
+        displayError("Please enter your last name.");
+        getLastName().focus();
+        getLastName().select();
+        return false;
+    }
+
+    if(password == "") {
+        displayError("Please enter your password.");
+        getPassword().focus();
+        getPassword().select();
+        return false; 
+    }
+
+    if(password != verifiedPassword) {
+        displayError("Passwords do not match. Re-enter your password.");
+        getVerifiedPassword().focus();
+        getVerifiedPassword().select();
+        return false; 
+    }
+    
+    return true; 
 }
+
+function saveActionClick(event) {
+    if(!validateForm()) {
+        return; 
+    }
+
+    const saveActionElement = event.target;
+	saveActionElement.disabled = true;
+
+    //const saveActionUrl = ("/api/employee");
+    
+    
+}
+
+// Getters
+function getSaveActionElement() {
+	return document.getElementById("saveButton");
+}
+
+function getFirstName() {
+    return document.getElementById("firstname");
+}
+
+function getLastName() {
+    return document.getElementById("lastname");
+}
+
+function getPassword() {
+    return document.getElementById("password");
+}
+
+function getVerifiedPassword() {
+    return document.getElementById("verifyPassword");
+}
+
+function getEmployeeType() {
+    return document.getElementById("classification");
+}
+
