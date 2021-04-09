301f7b5e5b1b74e2758db522c7d5408fa9b229b0
Jackson Bullard <jsbullar@uark.edu>
2021-04-08 20:35:14 -0500

    PARTIAL - Fixed the form and partially completed some of the JavaScript.

--- src/main/resources/static/scripts/employeeDetail.js
+++ src/main/resources/static/scripts/employeeDetail.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(event)
 {
-	document.getElementById("employeeID").style.display = "none";
-	document.getElementById("employeeIDLabel").style.display = "none";
+	getSaveActionElement().addEventListener("click", );
 });
 
-function validateForm()
+// Attempts to save user input
+function saveActionClick(event)
+{
+	if (!validateSave()) {
+		return;
+	}
+
+	const saveActionElement = event.target;
+	saveActionElement.disabled = true;
+
+
+}
+
+// Validates user input
+function validateSave()
 {
 	let firstname = document.forms["createEmployee"]["firstname"];
 	let lastname = document.forms["createEmployee"]["lastname"];
@@ -44,4 +57,96 @@ function validateForm()
 	{
 		return true;
 	}
+}
+
+// ----- GETTERS AND SETTERS ----- //
+
+// Gets the save button element
+function getSaveActionElement()
+{
+	return document.getElementById("saveButton");
+}
+
+// Gets the record ID
+function getRecordId()
+{
+	return getRecordIdElement().value;
+}
+
+// Gets the employee ID
+function getEmployeeId()
+{
+	return getEmployeeIdElement().value;
+}
+
+// Gets the first name
+function getFirstName()
+{
+	return getFirstNameElement().value;
+}
+
+// Gets the last name
+function getLastName()
+{
+	return getLastNameElement().value;
+}
+
+// Gets the password
+function getPassword()
+{
+	return getPasswordElement().value;
+}
+
+// Gets the verify password
+function getVerifyPassword()
+{
+	return getVerifyPasswordElement().value;
+}
+
+// Gets the employee classification
+function getEmployeeClass()
+{
+	return getEmployeeClassElement().value;
+}
+
+// Gets the record ID element
+function getRecordIdElement()
+{
+	return document.getElementById("recordId");
+}
+
+// Gets the employee ID element
+function getEmployeeIdElement()
+{
+	return document.getElementById("employeeId");
+}
+
+// Gets the first name element
+function getFirstNameElement()
+{
+	return document.getElementById("firstName");
+}
+
+// Gets the last name element
+function getLastNameElement()
+{
+	return document.getElementById("lastName");
+}
+
+// Gets the password element
+function getPasswordElement()
+{
+	return document.getElementById("password");
+}
+
+// Gets the verify password element
+function getVerifyPasswordElement()
+{
+	return document.getElementById("verifyPassword");
+}
+
+// Gets the employee classification element
+function getEmployeeClassElement()
+{
+	return document.getElementById("employeeClass");
 }
